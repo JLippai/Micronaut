a3eed08bb3c63074e589811d041647adaf17812f
aonanam <aonanam@bu.edu>
2018-04-25 20:53:09 -0400

    memory code functional, testbench implemented

--- memory_code/MemTest.v
+++ memory_code/MemTest.v
@@ -20,37 +20,12 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module MemTest(
-    input GCLK,
-    output reg LD0
-    //output reg [71:0] JoshLRead
-    //output reg [7:0] rgb
-    //input [ JoshLReadAddress
-    //input [63999:0] VGAReadAddress
-    //output reg count = 128'b0
+    input [12:0] readBlockAddress,
+    input [16:0] readByteAddress,
+    output [127:0] pixelBlockOut,
+    output [7:0] pixelOut
     );
-    reg [4799:0] readAddress = 4799'b0;     //19200 = 320 * 240 / 4 ... This is how many 4x4
-    wire [127:0] pixelblock;
     
-    frameMem frame1(GCLK, readAddress, pixelblock[127:96], pixelblock[95:64], pixelblock[63:32], pixelblock[31:0]);
-    
-    always@(posedge GCLK)
-    begin 
-        if(readAddress == {4799{1'b1}} && LD0==1'b0)
-        begin
-            readAddress = 0;
-            LD0 = 1'b1;
-        end
-        else if(readAddress == {4799{1'b1}} && LD0==1'b1)
-        begin
-            readAddress = 0;
-            LD0 = 1'b0;
-        end
-        else
-        begin
-            readAddress = readAddress + 3'd4;
-        end
-    end
-    
-    //assign pixelblock = count;
+    frameMem frame1(readBlockAddress, readByteAddress, pixelBlockOut[127:96], pixelBlockOut[95:64], pixelBlockOut[63:32], pixelBlockOut[31:0], pixelOut);
     
 endmodule
